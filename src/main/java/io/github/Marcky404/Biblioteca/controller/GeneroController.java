d739f2f819deb4f720682af4ab09006791387035
Ian Esquivel <ianalves908@gmail.com>
2024-02-02 18:20:40 -0300

    -feat: criado endpoint de atualizar por genero;

--- src/main/java/io/github/Marcky404/Biblioteca/controller/GeneroController.java
+++ src/main/java/io/github/Marcky404/Biblioteca/controller/GeneroController.java
@@ -1,6 +1,7 @@
 package io.github.Marcky404.Biblioteca.controller;
 
 import io.github.Marcky404.Biblioteca.domain.Genero;
+import io.github.Marcky404.Biblioteca.domain.request.GeneroRequest;
 import io.github.Marcky404.Biblioteca.service.GeneroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -35,5 +36,9 @@ public class GeneroController {
     }
 
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Genero> atualizar(@PathVariable("id") Long id, @RequestBody GeneroRequest generoRequest){
+        return ResponseEntity.ok(service.atualizar(id, generoRequest));
+    }
 
 }
