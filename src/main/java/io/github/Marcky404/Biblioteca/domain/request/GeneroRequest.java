a445841dc7b868480eef822b424d8f8e939db1f7
Ian Esquivel <ianalves908@gmail.com>
2024-02-02 17:38:44 -0300

    -feat: criado estrutura de crud para criação de genero

---
+++ src/main/java/io/github/Marcky404/Biblioteca/domain/request/GeneroRequest.java
@@ -0,0 +1,13 @@
+package io.github.Marcky404.Biblioteca.domain.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class GeneroRequest {
+
+    private String Genero;
+}
---
+++ src/main/java/io/github/Marcky404/Biblioteca/service/GeneroService.java
@@ -0,0 +1,37 @@
+package io.github.Marcky404.Biblioteca.service;
+
+import io.github.Marcky404.Biblioteca.domain.Genero;
+import io.github.Marcky404.Biblioteca.domain.request.GeneroRequest;
+import io.github.Marcky404.Biblioteca.repository.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GeneroService {
+
+    @Autowired
+    private GeneroRepository repository;
+
+    public void salvar(Genero genero) {
+        repository.save(genero);
+    }
+
+    public Genero buscar(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Genero não localizado!"));
+    }
+
+    @Transactional
+    public Genero atualizar(Long id, GeneroRequest generoRequest) {
+
+        Genero genero = buscar(id);
+        genero.setGenero(generoRequest.getGenero());
+
+        return genero;
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+}
