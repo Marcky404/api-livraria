52af1a9f296b5775468a02a8e794818e4fa894a7
Ian Esquivel <ianalves908@gmail.com>
2024-02-07 01:23:47 -0300

    -refactory: removido imports não usados.

--- src/main/java/io/github/Marcky404/Biblioteca/domain/response/ClienteResponse.java
+++ src/main/java/io/github/Marcky404/Biblioteca/domain/response/ClienteResponse.java
@@ -1,22 +1,13 @@
 package io.github.Marcky404.Biblioteca.domain.response;
 
 import io.github.Marcky404.Biblioteca.domain.Cliente;
-import io.github.Marcky404.Biblioteca.domain.Endereco;
-import io.github.Marcky404.Biblioteca.domain.Telefone;
 import io.github.Marcky404.Biblioteca.domain.enums.Sexo;
 import io.github.Marcky404.Biblioteca.domain.enums.Status;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
-
-import static io.github.Marcky404.Biblioteca.domain.request.EnderecoRequest.converterEnderecoParaParaLista;
-import static io.github.Marcky404.Biblioteca.domain.request.TelefoneRequest.converterParaLista;
 
 @Data
 @NoArgsConstructor
