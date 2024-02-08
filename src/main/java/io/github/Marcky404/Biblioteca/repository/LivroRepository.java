c734a6c09d3f3910e385e766c080f0c11d22f5a9
Ian Esquivel <ianalves908@gmail.com>
2024-02-07 23:38:27 -0300

    -feat: remoção de metodos e anotações não ultilizadas.

--- src/main/java/io/github/Marcky404/Biblioteca/repository/LivroRepository.java
+++ src/main/java/io/github/Marcky404/Biblioteca/repository/LivroRepository.java
@@ -1,6 +1,5 @@
 package io.github.Marcky404.Biblioteca.repository;
 
-import io.github.Marcky404.Biblioteca.domain.Genero;
 import io.github.Marcky404.Biblioteca.domain.Livro;
 import io.github.Marcky404.Biblioteca.domain.request.LivroQuantidadeTotalGenero;
 import org.springframework.data.jpa.repository.JpaRepository;
