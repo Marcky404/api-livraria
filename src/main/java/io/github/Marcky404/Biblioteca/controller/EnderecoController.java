1b772c0ee26c4b4fa2291e0922f3c8183570c4ea
Ian Esquivel <ianalves908@gmail.com>
2024-02-08 23:47:56 -0300

    -feat: criação do endpoint de atualizar na camada de controlle.

---
+++ src/main/java/io/github/Marcky404/Biblioteca/controller/EnderecoController.java
@@ -0,0 +1,24 @@
+package io.github.Marcky404.Biblioteca.controller;
+
+import io.github.Marcky404.Biblioteca.domain.request.EnderecoRequest;
+import io.github.Marcky404.Biblioteca.service.EnderecoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/endereco")
+public class EnderecoController {
+
+    private final EnderecoService service;
+
+
+    // RequestBody @Valid
+    @PutMapping("{id}")
+    public ResponseEntity<EnderecoRequest> atualizar(@PathVariable("id") Long id, @RequestBody @Valid EnderecoRequest enderecoRequest) {
+        return ResponseEntity.ok(service.atualizar(id, enderecoRequest));
+    }
+
+}
